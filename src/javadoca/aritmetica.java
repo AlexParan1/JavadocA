a09724e7832498772679f8ccbf05abe1c0f1fcf7
Alex Paran <alexparanlia@gmail.com>
2024-05-08 20:04:26 +0200

    primer commit

--- src/javadoca/aritmetica.java
+++ src/javadoca/aritmetica.java
@@ -8,20 +8,43 @@ package javadoca;
  */
 public class aritmetica {
 
-
+/**
+ * Sumamos dos numeros int (a y b)
+ * @param a es el primer numero
+ * @param b es el segundo numero
+ * @return la suma de a y b
+ */
     public int sumar(int a, int b) {
         return a + b;
     }
 
-
+/**
+ * 
+ * Restamos dos numeros int (a y b)
+ * @param a es el primer numero
+ * @param b es el segundo numero
+ * @return la resta de a y b
+ */
     public int restar(int a, int b) {
         return a - b;
     }
-
+/**
+ * 
+ * Multiplicamos dos numeros int (a y b)
+ * @param a es el primer numero
+ * @param b es el segundo numero
+ * @return la multiplicacion de a y b
+ */
     public int multiplicar(int a, int b) {
         return a * b;
     }
-
+/**
+ * 
+ * Dividimos dos numeros int (a y b)
+ * @param a es el primer numero
+ * @param b es el segundo numero
+ * @return la division de a y b o error si b es 0
+ */
     public double dividir(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("No se puede dividir por cero");
--- src/javadoca/operaciones.java
+++ src/javadoca/operaciones.java
@@ -8,6 +8,12 @@ package javadoca;
  */
 public class operaciones {
 
+    /**
+     * Tras introducir dos valores int (a y b)
+     * @param a representara el dividendo
+     * @param b representa el divisor
+     * @return error en caso de que b sea 0 o devuelve el resultado de la division de a por b
+     */
     public int modulo(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("El divisor no puede ser cero");
@@ -15,11 +21,19 @@ public class operaciones {
         return a % b;
     }
 
- 
+ /**
+  * Pide un int (a) (sera el lado de un cuadrado)
+  * @param a es el entero que deseemos
+  * @return a multiplicado por si mismo que sera el area del cuadrado
+  */
     public int calcularCuadrado(int a) {
         return a * a;
     }
-
+/**
+ * Pide un double (a) para calcular su raiz cuadrada
+ * @param a es el numero cuya raiz calculara
+ * @return error en caso de ser 0, si no es cero devolvera la raiz del numero elegido
+ */
     public double calcularRaizCuadrada(double a) {
         if (a < 0) {
             throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
